e00fd31673c89f4693110b6523a53d9ee8df7147
DennyMeyer <denny_meyer@gmx.net>
2021-11-03 23:20:44 +0100

    lab update

--- week_3/SQL_MYSQL/Labs/lab_work.sql
+++ week_3/SQL_MYSQL/Labs/lab_work.sql
@@ -42,3 +42,36 @@ using (address_id)
 where country = 'canada';
 
 ### 6. Which are films starred by the most prolific actor? Most prolific actor is defined as the actor that has acted in the most number of films. First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.
+
+select title from sakila.film
+where film_id in (
+select film_id from sakila.film_actor 
+where actor_id = (
+select max(actor_id) from sakila.film_actor
+inner join
+	sakila.film
+using (film_id)
+inner join
+	sakila.actor
+using (actor_id)
+));
+
+
+select total, movie from (
+select count(actor_id) as total, film_id as movie from
+sakila.film_actor
+group by 
+	film_id) x
+order by total desc ;
+
+
+select title, movie_star from film
+where film_id in (
+select film_id, movie_star from sakila.film_actor 
+where actor_id = (
+select actor_id as movie_star from sakila.film_actor
+group by 
+	actor_id 
+order by 
+	count(actor_id) desc limit 1));
+  
